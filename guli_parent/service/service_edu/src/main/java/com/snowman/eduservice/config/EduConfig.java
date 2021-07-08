eb4e3fcd66219f7e569bbe5d8d5e286ec8cc8305
sn0wm1n <592549438@qq.com>
2021-07-08 11:27:13 +0800

    7-8:讲师CRUD接口开发完了

--- guli_parent/service/service_edu/src/main/java/com/snowman/eduservice/config/EduConfig.java
+++ guli_parent/service/service_edu/src/main/java/com/snowman/eduservice/config/EduConfig.java
@@ -2,6 +2,7 @@ package com.snowman.eduservice.config;
 
 import com.baomidou.mybatisplus.core.injector.ISqlInjector;
 import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -32,4 +33,15 @@ public class EduConfig {
     public ISqlInjector sqlInjector() {
         return new LogicSqlInjector();
     }
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+
+
 }
--- guli_parent/service/service_edu/src/main/java/com/snowman/eduservice/controller/EduTeacherController.java
+++ guli_parent/service/service_edu/src/main/java/com/snowman/eduservice/controller/EduTeacherController.java
@@ -1,9 +1,17 @@
 package com.snowman.eduservice.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.snowman.commonutils.R;
 import com.snowman.eduservice.entity.EduTeacher;
+import com.snowman.eduservice.entity.VO.TeacherQuery;
 import com.snowman.eduservice.service.EduTeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,6 +24,7 @@ import java.util.List;
  * @author snowman
  * @since 2021-07-06
  */
+@Api(tags = "讲师管理")
 @RestController
 @RequestMapping("/eduservice/edu-teacher")
 public class EduTeacherController {
@@ -26,17 +35,114 @@ public class EduTeacherController {
 
     //1.查询讲师表所有数据
     //rest风格
+    @ApiOperation(value = "所有讲师列表")
     @GetMapping("findAll")
-    public List<EduTeacher> findAllTeacher(){
+    public R findAllTeacher() {
         List<EduTeacher> list = teacherService.list(null);
-        List<EduTeacher> list1 = list;
-        return list1;
+
+
+        return R.ok().data("items", list);
     }
     //2.逻辑删除讲师的方法
-    @DeleteMapping("{id}")
-    public boolean RemoveTeacher(@PathVariable String id){
-        return teacherService.removeById(id);
 
+    @ApiOperation(value = "根据ID逻辑删除讲师")
+    @DeleteMapping("{id}")
+    public R RemoveTeacher(
+            @ApiParam(name = "id", value = "讲师ID", required = true)
+            @PathVariable String id) {
+        teacherService.removeById(id);
+        return R.ok();
+
+    }
+
+    //3.分页查询讲师
+    @ApiOperation(value = "分页查询讲师")
+    @GetMapping("pageTeacher/{current}/{limit}")
+    public R pageListTeacher(
+            @PathVariable long current,
+            @PathVariable long limit
+    ) {
+
+        Page<EduTeacher> pageTeacher = new Page<>(current, limit);
+        teacherService.page(pageTeacher, null);
+
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records = pageTeacher.getRecords();
+        return R.ok().data("total", total).data("rows", records);
+
+    }
+
+    //4.添加讲师的方法
+    @ApiOperation(value = "条件查询带分页的方法")
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageTeacherCondition(
+            @PathVariable long current,
+            @PathVariable long limit,
+            @RequestBody(required = false) TeacherQuery teacherQuery) {
+        Page<EduTeacher> pageTeacher = new Page<>(current, limit);
+
+
+        //service
+        QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+
+        if (!StringUtils.isEmpty(name)) {
+            wrapper.like("name", name);
+        }
+        if (!StringUtils.isEmpty(level)) {
+            wrapper.eq("level", level);
+        }
+        if (!StringUtils.isEmpty(begin)) {
+            wrapper.ge("gmt_create", begin);
+        }
+        if (!StringUtils.isEmpty(end)) {
+            wrapper.le("gmt_modified", end);
+        }
+
+        teacherService.page(pageTeacher, wrapper);
+//
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records = pageTeacher.getRecords();
+
+        return R.ok().data("total", total).data("rows", records);
+    }
+
+    //5.添加讲师的方法
+    @ApiOperation("添加讲师的方法")
+    @PostMapping("addTeacher")
+    public R addTeacher(@RequestBody EduTeacher eduTeacher) {
+
+        boolean save = teacherService.save(eduTeacher);
+
+        if (save) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
+    }
+
+    //6.根据讲师id查询
+    @ApiOperation("根据讲师id查询")
+    @GetMapping("getTeacher/{id}")
+    public R getTeacher(@PathVariable String id){
+        EduTeacher eduTeacher = teacherService.getById(id);
+
+        return R.ok().data("teacher",eduTeacher);
+    }
+    //7.根据讲师id修改
+    @ApiOperation("根据讲师id修改")
+    @PostMapping("updateTeacher")
+    public R updateTeacher(@RequestBody EduTeacher eduTeacher){
+
+        boolean flag = teacherService.updateById(eduTeacher);
+        if (flag){
+            return R.ok();
+        }else {
+            return R.error();
+        }
     }
 
 
